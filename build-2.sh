230d004ecf22443a0b71cdb111dcb64daa82705b
Growtopia Jaw <growtopiajaw@gmail.com>
2022-07-09 13:31:37 +0000

    Update

--- build-2.sh
+++ build-2.sh
@@ -1,3 +1,3 @@
 #! /usr/local/bin/bash
 
-for i in $(cat macos-11.txt); do cd /Applications/Xcode_"$i".app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs; for j in "$(ls | grep -v MacOSX.sdk)"; do sudo tar -cvhf /Users/runner/work/MacOSX-SDK/MacOSX-SDK/xcode/"$j".tar "$j"; done; done
+for i in $(cat macos-11.txt); do cd /Applications/Xcode_"$i".app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs; for j in "$(ls | grep MacOSX | grep -v MacOSX.sdk)"; do sudo tar -cvhf /Users/runner/work/MacOSX-SDK/MacOSX-SDK/xcode/"$j".tar "$j"; done; done
