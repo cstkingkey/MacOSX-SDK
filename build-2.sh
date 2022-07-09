523d75ba44ba7d5c79ab014c2eb66e03c4d698bc
Growtopia Jaw <growtopiajaw@gmail.com>
2022-07-09 20:45:13 +0800

    Create build-2.sh

---
+++ build-2.sh
@@ -0,0 +1,3 @@
+#! /usr/local/bin/bash
+
+for i in $(cat macos-11.txt); do cd /Applications/Xcode_"$i".app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs; for j in "$(ls | grep -v MacOSX.sdk)"; do sudo tar -cvhf /Users/runner/work/iPhoneOS-SDK/iPhoneOS-SDK/xcode/"$j".tar "$j"; done; done
