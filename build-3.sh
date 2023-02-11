f46a37e13d37e9d8e2a8ef7ee36f660073233f8f
cstkingkey <cstkingkey@users.noreply.github.com>
2023-02-11 15:23:19 +0800

    Update build-3.sh

--- build-3.sh
+++ build-3.sh
@@ -1,3 +1,9 @@
 #! /usr/local/bin/bash
 
-for i in $(cat macos-12.txt); do cd /Applications/Xcode_"$i".app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs; ls -la; for j in "$(ls | grep MacOSX | grep -v MacOSX.sdk)"; do sudo tar -cJvhf /Users/runner/work/MacOSX-SDK/MacOSX-SDK/xcode/"$j".tar.xz "$j"; done; done
+for i in $(cat macos-12.txt); do
+      cd /Applications/Xcode_"$i".app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs
+      ls -la
+      for j in "$(ls | grep MacOSX | grep -v MacOSX.sdk)"; do
+            sudo tar -cJvhf /Users/runner/work/MacOSX-SDK/MacOSX-SDK/xcode/"$j".tar.xz "$j"
+      done
+done
