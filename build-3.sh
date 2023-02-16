11b7e52cbf42895379f43ebd97db8c90a1cf0808
cstkingkey <cstkingkey@users.noreply.github.com>
2023-02-16 21:56:01 +0800

    Update build-3.sh

--- build-3.sh
+++ build-3.sh
@@ -3,7 +3,8 @@
 for i in $(cat macos-12.txt); do
     cd /Applications/Xcode_"$i".app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs
     ls -la
+    rm -rf MacOSX13.sdk
     for j in "$(ls | grep MacOSX | grep -v MacOSX.sdk)"; do
-        sudo tar -cJvf /Users/runner/work/MacOSX-SDK/MacOSX-SDK/xcode/"$j".tar.xz "$j"
+        sudo tar -cJvhf /Users/runner/work/MacOSX-SDK/MacOSX-SDK/xcode/"$j".tar.xz "$j"
     done
 done
