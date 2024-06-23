53934810343ba97f6034dfbe392300e11d83faf7
John <m@DESKTOP-7TRGB04.localdomain>
2024-06-23 11:45:59 +0800

    new

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #! /usr/local/bin/bash
 
-for i in $(cat macos-10.15.txt); do cd /Applications/Xcode_"$i".app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs; for j in "$(ls | grep MacOSX | grep -v MacOSX.sdk)"; do sudo tar -cJvhf /Users/runner/work/MacOSX-SDK/MacOSX-SDK/xcode/"$j".tar.xz "$j"; done; done
+for i in $(cat ${config}); do
+    cd /Applications/Xcode_"$i".app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs
+    ls -la
+    rm -rf MacOSX13.sdk
+    rm -rf MacOSX14.sdk
+    rm -rf MacOSX15.sdk
+    for j in "$(ls | grep MacOSX | grep -v MacOSX.sdk)"; do
+        sudo tar -cJvhf /Users/runner/work/MacOSX-SDK/MacOSX-SDK/xcode/"$j".tar.xz "$j"
+    done
+done
